fb011b5ebecf5556afa6cfc338bd646d912b38cb
antmaster2001 <gijsbakker@hotmail.nl>
2021-06-30 18:14:44 +0200

    Extra testdata

--- src/renderer/Components/DisplayComponents/ProgramsDisplay.tsx
+++ src/renderer/Components/DisplayComponents/ProgramsDisplay.tsx
@@ -3,8 +3,8 @@ import React from "react";
 const ProgramsDisplay = () => {
 
 	const gameTypes : any = {
-		1: "game",
-		2: "browser"
+		1: "Game",
+		2: "Browser"
 	}
 
 	const programTestData = [
@@ -12,7 +12,22 @@ const ProgramsDisplay = () => {
 			icon: "https://theme.zdassets.com/theme_assets/518064/9766b1eef40938c1aeab5838af0a06a29647d7eb.png",
 			name: "Sea of thieves",
 			type: 1
-		}
+		},
+		{
+			icon: "https://cdn.icon-icons.com/icons2/2699/PNG/512/minecraft_logo_icon_168974.png",
+			name: "Minecraft",
+			type: 1
+		},
+		{
+			icon: "https://upload.wikimedia.org/wikipedia/commons/8/87/Google_Chrome_icon_%282011%29.png",
+			name: "Chrome",
+			type: 2
+		},
+		{
+			icon: "https://cdn2.iconfinder.com/data/icons/atrous/512/speaker_volume-512.png",
+			name: "System sounds",
+		},
+		
 	]
 
 	return (
